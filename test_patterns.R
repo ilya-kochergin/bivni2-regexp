dced5ecbcb370a32f0c45394ca74679f654a9d02
ilya-kochergin <ikochergin@gmail.com>
2015-12-03 17:29:29 +0300

    test_pattern

--- test_patterns.R
+++ test_patterns.R
@@ -8,11 +8,13 @@ cat(pattern1)
 print(pattern1)
 
 pattern2 <- "<dd>[Ff]ound on (.+?)</dd>"
+date_pos0 <- regexpr(pattern = pattern2,homicides)
 date_pos <- regexec(pattern = pattern2,homicides)
 sapply(X = regmatches(homicides,date_pos), FUN = function(x) x[2])
+sapply(X = regmatches(homicides,date_pos0), FUN = function(x) x[2])
 
 
 test_pattern <- function(pat,text,...) {
-  pos <- gregexpr(pat,text)
-  sapply(X = regmatches(text,oos), FUN = function(x) x[2])
+  pos <- regexec(pat,text,useBytes = FALSE)
+  sapply(X = regmatches(text,pos), FUN = function(x) x[2])
 }
