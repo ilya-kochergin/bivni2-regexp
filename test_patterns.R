1fef8b9dd23516ccbc1ecbdb6ed685caaaac7ec8
Ilya Kochergin <tigro2004@mail.ru>
2015-12-03 18:56:55 +0300

    konec seminara

--- test_patterns.R
+++ test_patterns.R
@@ -8,12 +8,20 @@ cat(pattern1)
 print(pattern1)
 
 pattern2 <- "<dd>[Ff]ound on (.+?)</dd>"
+  
+whole_pos <- regexpr(pattern2, homicides)
+
+regmatches(homicides,m = whole_pos)[1:5]
+
+pattern2 <- "<dd>[Ff]ound on (.+?)</dd>"
 date_pos0 <- regexpr(pattern = pattern2,homicides)
 date_pos <- regexec(pattern = pattern2,homicides)
-sapply(X = regmatches(homicides,date_pos), FUN = function(x) x[2])
+date_text <- sapply(X = regmatches(homicides,date_pos), FUN = function(x) x[2])
 sapply(X = regmatches(homicides,date_pos0), FUN = function(x) x[2])
 
+date_text
 
+as.Date(x = date_text, format="%B %d, %y")
 test_pattern <- function(pat,text,...) {
   pos <- regexec(pat,text,useBytes = FALSE)
   sapply(X = regmatches(text,pos), FUN = function(x) x[2])
